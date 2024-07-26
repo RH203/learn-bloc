94b70ba4f348239888a03cfce77ed5a4b87bbe07
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-07-26 15:56:19 +0700

    chore: Update main.dart to use App widget

--- timer_app/lib/main.dart
+++ timer_app/lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:timer_app/app.dart';
+import 'package:timer_app/observer.dart';
 
 void main() {
+  Bloc.observer = const Observer();
   runApp(const App());
 }
