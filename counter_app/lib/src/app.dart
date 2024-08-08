1c1a74d26cfc9a91d6487f9375286565096736d4
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-08-08 14:50:43 +0700

    chore: Update App to use MultiBlocProvider for managing multiple BLoCs

--- counter_app/lib/src/app.dart
+++ counter_app/lib/src/app.dart
@@ -1,15 +1,24 @@
+import 'package:counter_app/src/features/counter/bloc/counter_bloc.dart';
 import 'package:counter_app/src/features/counter/view/counter_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: "Counter APP",
-      debugShowCheckedModeBanner: false,
-      home: CounterApp(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<CounterBloc>(
+          create: (context) => CounterBloc(),
+        )
+      ],
+      child: const MaterialApp(
+        title: "Counter APP",
+        debugShowCheckedModeBanner: false,
+        home: CounterApp(),
+      ),
     );
   }
 }
